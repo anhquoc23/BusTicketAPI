c55a71cb83ef038e569fadfbc97895c5cd40f271
anhquoc23 <nguyenah316@gmail.com>
2024-02-02 09:20:09 +0700

    feat: Create reactjs app

---
+++ react-bus/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element=''></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
